8e055090057c55e6622dd858e4c22cae1172ac59
Angel Huizar <ah426@duke.edu>
2021-05-11 12:58:38 -0700

    dockerized the react app

---
+++ react_app/src/App.js
@@ -0,0 +1,11 @@
+import Translate from "./components/Translate";
+
+function App() {
+  return (
+    <div className="bg-secondary" style={{ height: "200px" }}>
+      <Translate />
+    </div>
+  );
+}
+
+export default App;
--- react_app/src/components/Translate.jsx
+++ react_app/src/components/Translate.jsx
@@ -1,47 +1,67 @@
-import React, { useState } from 'react';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React, { useState } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function Translate() {
-  const [text, setText] = useState('');
-  const [language, setLanguage] = useState({ libre: 'en', speechSynth: 'en-US', display: 'English' });
+  const [text, setText] = useState("");
+  const [language, setLanguage] = useState({
+    libre: "en",
+    speechSynth: "en-US",
+    display: "English",
+  });
+  const [loading, setLoading] = useState(false);
   const onChangeHandler = (e) => {
     setText(e.target.value);
   };
   const onSubmit = async () => {
-    console.log('clicked', text);
-    const res = await fetch('https://libretranslate.com/translate', {
-      method: 'POST',
+    console.log("text: ", text);
+    setLoading(true);
+    const res = await fetch("http://localhost:5000/translate", {
+      method: "POST",
       body: JSON.stringify({
         q: text,
-        source: 'en',
+        source: "en",
         target: language.libre,
       }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
     let translateText = await res.json();
     translateText = translateText.translatedText;
+    setLoading(false);
     console.log(translateText);
     const utterance = new SpeechSynthesisUtterance(translateText);
-    const voices = window.speechSynthesis.getVoices();
+    // const voices = window.speechSynthesis.getVoices();
     // eslint-disable-next-line prefer-destructuring
-    utterance.voice = voices.filter((voice) => voice.name === 'Alex')[0];
+    // utterance.voice = voices.filter((voice) => voice.name === "Alex")[0];
     utterance.lang = language.speechSynth;
+    utterance.volume = 1;
+    utterance.rate = 1;
+    utterance.pitch = 1;
     window.speechSynthesis.speak(utterance);
   };
   const handleChange = (e) => {
     setLanguage(e.target.value);
-    console.log(e);
   };
   return (
     <div className="bg-light">
       <div className="container">
         <input type="text" onChange={onChangeHandler} />
-        <button type="button" className="btn btn-primary" onClick={onSubmit}>
-          Translate
-        </button>
+        <>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={onSubmit}
+            >
+              Translate
+            </button>
+          )}
+        </>
       </div>
       <div className="container">
         <FormControl>
@@ -55,63 +75,63 @@ export default function Translate() {
           >
             <MenuItem
               value={{
-                libre: 'en',
-                speechSynth: 'en-US',
-                display: 'English',
+                libre: "en",
+                speechSynth: "en-US",
+                display: "English",
               }}
             >
               English
             </MenuItem>
             <MenuItem
               value={{
-                libre: 'es',
-                speechSynth: 'es-ES',
-                display: 'Spanish',
+                libre: "es",
+                speechSynth: "es-ES",
+                display: "Spanish",
               }}
             >
               Spanish
             </MenuItem>
             <MenuItem
               value={{
-                libre: 'zh',
-                speechSynth: 'zh-HK',
-                display: 'Chinese-HK',
+                libre: "zh",
+                speechSynth: "zh-CN",
+                display: "Chinese-CN",
               }}
             >
-              Chinese-HK
+              Chinese-CN
             </MenuItem>
             <MenuItem
               value={{
-                libre: 'hi',
-                speechSynth: 'hi-IN',
-                display: 'Hindi',
+                libre: "hi",
+                speechSynth: "hi-IN",
+                display: "Hindi",
               }}
             >
               Hindi
             </MenuItem>
             <MenuItem
               value={{
-                libre: 'fr',
-                speechSynth: 'fr-FR',
-                display: 'French',
+                libre: "fr",
+                speechSynth: "fr-FR",
+                display: "French",
               }}
             >
               French
             </MenuItem>
             <MenuItem
               value={{
-                libre: 'de',
-                speechSynth: 'de-DE',
-                display: 'German',
+                libre: "de",
+                speechSynth: "de-DE",
+                display: "German",
               }}
             >
               German
             </MenuItem>
             <MenuItem
               value={{
-                libre: 'ja',
-                speechSynth: 'ja-JP',
-                display: 'Japanese',
+                libre: "ja",
+                speechSynth: "ja-JP",
+                display: "Japanese",
               }}
             >
               Japanese
@@ -119,16 +139,6 @@ export default function Translate() {
           </Select>
         </FormControl>
       </div>
-      <button
-        type="button"
-        className="btn btn-warning"
-        onClick={() => {
-          const audio = new Audio('../../public/trans.mp3');
-          audio.play();
-        }}
-      >
-        Play text
-      </button>
     </div>
   );
 }
